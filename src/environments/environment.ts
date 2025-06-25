46be0841f1dfeecdee88e2541629640e8e7fe231
Gevko <oleksandr.gevko@siemens.com>
2025-06-25 20:04:24 +0100

    new deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,3 @@ export const environment = {
   googleApiKey: 'AIzaSyAYsQdbV24tP9O1L_c7cr1iwULNeTIjEn0',
   googleCalendarId: 'thehouseofgaeiras@gmail.com'
 };
-
-// src/environments/environment.prod.ts
-// export const environment = {
-//   production: true,
-//   googleApiKey: 'YOUR_GOOGLE_API_KEY',
-//   googleCalendarId: 'YOUR_CALENDAR_ID@group.calendar.google.com'
-// };
